8545e88584c17350a3dc64f7ca9493b5da30beb5
rgreenblatt <greenblattryan@gmail.com>
2021-12-10 02:11:57 -0500

    added clock to utils

--- gpu_code_temp/include/utils.h
+++ gpu_code_temp/include/utils.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <optional>
 
 #include <cuda_runtime.h>
 
@@ -20,3 +23,39 @@ inline void cuda_assert(cudaError_t code, const char *file, int line) {
     CUDA_ERROR_CHK(cudaPeekAtLastError());                                     \
     CUDA_ERROR_CHK(cudaDeviceSynchronize());                                   \
   } while (0)
+
+class Timer {
+public:
+  using Clock = std::chrono::high_resolution_clock;
+  using TimePoint = Clock::time_point;
+
+  Timer() { start(); }
+  Timer(std::optional<TimePoint> start) : start_(start) {}
+
+  double elapsed() {
+    if (start_.has_value()) {
+      return total_ + std::chrono::duration_cast<std::chrono::duration<double>>(
+                          now() - *start_)
+                          .count();
+    } else {
+      return total_;
+    }
+  }
+
+  void stop() {
+    total_ = elapsed();
+    start_ = std::nullopt;
+  }
+
+  void start() { start_ = now(); }
+
+  void report(const std::string &name) {
+    std::cout << name << ": " << elapsed() << std::endl;
+  }
+
+private:
+  static TimePoint now() { return Clock::now(); }
+
+  std::optional<TimePoint> start_ = std::nullopt;
+  double total_ = 0.;
+};
