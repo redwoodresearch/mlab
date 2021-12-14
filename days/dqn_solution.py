5468fc688a1d0ae4df2a726930370bb7e497410e
rgreenblatt <greenblattryan@gmail.com>
2021-12-14 11:23:51 -0800

    soem updates

--- days/dqn_solution.py
+++ days/dqn_solution.py
@@ -1,49 +1,32 @@
+from comet_ml import Experiment
+
+import sys
+from math import prod
+
+import gin
 import torch
 import numpy as np
 from torch import nn
-from torch.optim import Adam
+from torch.optim import Adam, lr_scheduler
 import gym
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-env = gym.make("CartPole-v0")
 
-hidden_size = 16
-
-net = nn.Sequential(
-    nn.Linear(4, hidden_size),
-    nn.ReLU(),
-    nn.Linear(hidden_size, hidden_size),
-    nn.ReLU(),
-    nn.Linear(hidden_size, 2),
-)
-loss_fn = nn.MSELoss(reduction='mean')
-optim = Adam(net.parameters(), lr=1e-2)
-
-discount = 0.95
-episodes = 10000
-eps = 0.05
-
-obs_frac = 0.2
-
-losses = []
-durations = []
-
-for i in range(episodes):
+def run_episode(env, net, eps, render=False):
     obs = env.reset()
 
     states = []
     rewards = []
     choices = []
-    was_eps = []
 
     with torch.no_grad():
         while True:
+            if render:
+                env.render()
             states.append(obs)
             if torch.rand(()) < eps:
-                was_eps.append(True)
                 choice = env.action_space.sample()
             else:
-                was_eps.append(False)
                 expected_reward = net(torch.tensor(obs))
                 choice = torch.argmax(expected_reward).numpy()
             choices.append(choice)
@@ -52,42 +35,82 @@ for i in range(episodes):
             if done:
                 break
 
-    for j in reversed(range(len(rewards) - 1)):
-        rewards[j] += discount * rewards[j + 1]
-
-    episode_len = len(rewards)
-
-    states = torch.tensor(np.array(states))
-    rewards = torch.tensor(rewards)
-    choices = torch.tensor(np.array(choices))
-
-    if i == 9999:
-        print(rewards)
-        print(choices)
-        print(net(states))
-        print(net(states)[torch.arange(states.size(0)), choices])
-        print(was_eps)
-
-    subset = torch.randint(episode_len, (round(obs_frac * episode_len),))
-
-    states = states[subset]
-    rewards = rewards[subset]
-    choices = choices[subset]
-
-    loss = loss_fn(rewards, net(states)[torch.arange(states.size(0)), choices])
-
-    optim.zero_grad()
-    loss.backward()
-    optim.step()
-
-    if i % 1000 == 0:
-        print(episode_len)
+    return states, rewards, choices
 
 
-    losses.append(loss.item())
-    durations.append(episode_len)
+@gin.configurable
+def train_dqn(experiment_name, env_id, gamma, episodes, start_eps, end_eps,
+              hidden_size, start_lr, end_lr, train_frac_per_episode):
+    # Create an experiment with your api key
+    experiment = Experiment(
+        api_key="gDeuTHDCxQ6xdsXnvzkWsvDEb",
+        project_name=f"train_dqn",
+        workspace="rgreenblatt",
+    )
+    experiment.set_name(experiment_name)
 
-plt.plot(losses)
-plt.show()
-plt.plot(durations)
-plt.show()
+    env = gym.make(env_id)
+    net = nn.Sequential(
+        nn.Linear(prod(env.observation_space.shape), hidden_size),
+        nn.ReLU(),
+        nn.Linear(hidden_size, hidden_size),
+        nn.ReLU(),
+        nn.Linear(hidden_size, env.action_space.n),
+    )
+    loss_fn = nn.MSELoss(reduction='sum')
+
+    optim = Adam(net.parameters(), lr=start_lr)
+    scheduler = lr_scheduler.ExponentialLR(optim, gamma=(end_lr / start_lr)**(1 / episodes))
+
+    losses = []
+    durations = []
+
+    for eps_idx in tqdm(range(episodes)):
+        eps = (end_eps - start_eps) * eps_idx / episodes + start_eps
+        states, rewards, choices = run_episode(env, net, eps)
+
+        total_reward = sum(rewards)
+
+        # maybe clean way to do this as a vectorized operation?
+        for j in reversed(range(len(rewards) - 1)):
+            rewards[j] += gamma * rewards[j + 1]
+
+        episode_len = len(rewards)
+
+        states = torch.tensor(np.array(states))
+        rewards = torch.tensor(rewards)
+        choices = torch.tensor(np.array(choices))
+
+        subset = torch.randperm(episode_len)[:round(train_frac_per_episode *
+                                                    episode_len)]
+
+        states = states[subset]
+        rewards = rewards[subset]
+        choices = choices[subset]
+
+        loss = loss_fn(rewards,
+                       net(states)[torch.arange(states.size(0)), choices])
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        scheduler.step()
+
+        experiment.log_metric("episode len", episode_len)
+        experiment.log_metric("total reward", total_reward)
+        experiment.log_metric("eps", eps)
+        experiment.log_metric("lr", scheduler.get_last_lr()[0])
+
+        losses.append(loss.item())
+        durations.append(episode_len)
+
+    run_episode(env, net, eps=0, render=True)
+
+
+def main():
+    gin.parse_config_file(sys.argv[1])
+    train_dqn()
+
+
+if __name__ == "__main__":
+    main()
