bbd0127b51a74df20fb065e732a5da0167f4eb2d
rgreenblatt <greenblattryan@gmail.com>
2022-01-02 15:22:51 -0800

    Also install snap

--- lambda-labs-setup.sh
+++ lambda-labs-setup.sh
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 pip install -e .
 pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
 mkdir ~/working
-sudo apt-get install -y ffmpeg
+sudo apt-get install -y ffmpeg snap
 sudo snap install code --classic
 code --install-extension ms-python.python
 code --install-extension ms-toolsai.jupyter
