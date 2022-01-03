3efa33b642287c055602ab765542199a3b56014e
Tao Lin <taoroalin@gmail.com>
2022-01-03 15:06:32 -0800

    wall

--- ll_pull.sh
+++ ll_pull.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo $1
-ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh $1 "cd ~/mlab && git pull"
+ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh $1 "cd ~/mlab && git pull -s recursive -X ours && wall 'UPDATED GIT REPO'"
