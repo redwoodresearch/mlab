118fd78dc1c513e36ee464ab5570785af67cbfbf
mlab people <mlab@rdwrs.com>
2022-01-07 20:11:31 +0000

    run experiment

--- days/w1d4/run_experiment.py
+++ days/w1d4/run_experiment.py
@@ -13,7 +13,6 @@ import gin
 import os
 os.system("pip install -r ../../requirements.txt")
 
-from .w1d4_template import run_experiment
 data_train, data_test = w1d4_tests.load_image('pineapple.jpg')
 
 @gin.configurable
@@ -23,32 +22,23 @@ def train(model: nn.Module, dataloader, lr, momentum, experiment=None, epochs=1)
         if experiment is not None:
             experiment.log_metric('val loss', sol._evaluate(model, dataloader))
 
-def make_grid(possible_values):
-    possible_values = possible_values.items()
-    result = [{}]
-    for k, vals in possible_values:
-        result = [dict(**x, **{k: v}) for x in result for v in vals]
-    return result
 
 # print(make_grid(possible_values))
 
 def run_experiment(params):
     with gin.unlock_config():
-        gin.parse_config_files_and_bindings([], [f'{k}={v}' for k,v in params.items()])
+        gin.parse_config_files_and_bindings([], params.split('\n'))
         # Create an experiment with your api key
         experiment = Experiment(
             api_key="oGcm04SiEeJM89dRyU0vcOFzd",
-            project_name="hyperparameters4",
+            project_name="hyperparameters5",
             workspace="luuf",
         )
-        for k, v in params.items():
-            assert isinstance(k, str)
-            print(k)
-            print(v)
-            experiment.log_parameter(k, v)
+        experiment.log_parameter('hyperparameters', params)
         m = sol._MLP(in_dim=2, hidden_dim=400, out_dim=3)
         train(m, data_train)
         experiment.end()
 
 
 run_experiment(os.environ['PARAMS'].gin_config)
+# run_experiment('train.lr=.01\ntrain.epochs=1\ntrain.momentum=.9')
