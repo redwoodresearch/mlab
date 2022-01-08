996b7344c98bfda5ce2fcefb6dfc89d25191db9f
Daniel Ziegler <ziedaniel1@gmail.com>
2022-01-08 10:42:41 -0800

    intro

--- days/w1d6/intro.py
+++ days/w1d6/intro.py
@@ -10,7 +10,7 @@ mod = load_module('intro.cu')
 zero_kernel = mod.get_function('zero')
 one_kernel = mod.get_function('one')
 dest = torch.ones(128, dtype=torch.float32).cuda()
-zero_kernel(Holder(dest), block=(64, 1, 1), grid=(1, 1))
+zero_kernel(Holder(dest), block=(128, 1, 1), grid=(1, 1, 1))
 
 # Kernels run async by default, so we call synchronize() before using values.
 torch.cuda.synchronize()
