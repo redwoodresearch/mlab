77ab7bf791ab992acaab6fac0b04b57f65a7236e
Tom Tseng <tom.hm.tseng@gmail.com>
2022-01-10 23:23:54 -0800

    w2d1d2: Copy over from branch nat_tom_w2d1

---
+++ days/w2d1d2/bert_tao.py
@@ -0,0 +1,296 @@
+import torch as t
+import numpy as np
+from torch.nn import (
+    Module,
+    Parameter,
+    Sequential,
+)  # not allowed to use other stuff from nn
+from transformers import AutoTokenizer
+
+# from days.modules import gelu, Embedding, Dropout, LayerNorm, softmax, Linear
+from torch.nn import Embedding, Dropout, LayerNorm, Linear
+from torch.nn.functional import gelu, softmax
+
+from einops import rearrange
+from utils import tpeek, copy_weight_bias
+from dataclasses import dataclass
+
+
+class BertEmbedding(Module):
+    def __init__(self, config):
+        super(BertEmbedding, self).__init__()
+        self.config = config
+        embedding_size = config["hidden_size"]
+        # this needs to be initialized as a bunch of normalized vector,
+        # as opposed to a linear layer, which is initialized to _produce_ normalized vectors
+        self.token_embedding = Embedding(config["vocab_size"], embedding_size)
+        self.position_embedding = Embedding(
+            config["max_position_embeddings"], embedding_size
+        )
+        self.token_type_embedding = Embedding(config["type_vocab_size"], embedding_size)
+
+        self.layer_norm = LayerNorm((embedding_size,))
+        self.dropout = Dropout(config["dropout"])
+
+    def embed(self, input_ids: t.LongTensor, token_type_ids):
+        seq_length = input_ids.shape[1]
+        token_embeddings = self.token_embedding(input_ids)
+        token_type_embeddings = self.token_type_embedding(token_type_ids)
+        position_embeddings = self.position_embedding(
+            t.arange(seq_length).to(next(self.parameters()).device)
+        )
+        embeddings = token_embeddings + token_type_embeddings + position_embeddings
+        embeddings = self.layer_norm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
+
+    def forward(self, **kwargs):
+        return self.embed(**kwargs)
+
+    def unembed(self, embeddings: t.Tensor):
+        return self.token_embedding.unembed(embeddings)
+
+
+def bert_mlp(token_activations, linear_1, linear_2, dropout):
+    return dropout(linear_2(gelu(linear_1(token_activations))))
+
+
+class NormedResidualLayer(Module):
+    def __init__(self, size, intermediate_size, dropout):
+        super(NormedResidualLayer, self).__init__()
+        self.mlp1 = Linear(size, intermediate_size, bias=True)
+        self.mlp2 = Linear(intermediate_size, size, bias=True)
+        self.layer_norm = LayerNorm((size,))
+        self.dropout = Dropout(dropout)
+
+    def forward(self, input):
+        intermediate = gelu(self.mlp1(input))
+        output = self.dropout(self.mlp2(intermediate)) + input
+        output = self.layer_norm(output)
+        return output
+
+
+def raw_attention_pattern(
+    token_activations, num_heads, project_query, project_key, attention_mask=None
+):
+    head_size = token_activations.shape[-1] // num_heads
+
+    query = project_query(token_activations)
+    query = rearrange(query, "b s (h c) -> b h s c", h=num_heads)
+
+    key = project_key(token_activations)
+    key = rearrange(key, "b s (h c) -> b h s c", h=num_heads)
+
+    # my attention raw has twice the mean and half the variance of theirs
+    attention_raw = t.einsum("bhtc,bhfc->bhft", query, key) / np.sqrt(head_size)
+    if attention_mask is not None:
+        attention_raw -= (1 - attention_mask) * 10000
+    return attention_raw
+
+
+def multi_head_self_attention(
+    token_activations, num_heads, attention_pattern, project_value, project_out, dropout
+):
+
+    # if attention_masks is not None:
+    #     attention_raw = attention_raw * attention_masks
+    attention_patterns = softmax(attention_pattern, dim=-2)
+    attention_patterns = dropout(attention_patterns)
+
+    value = project_value(token_activations)
+    value = rearrange(value, "b s (h c) -> b h s c", h=num_heads)
+
+    context_layer = t.einsum("bhft,bhfc->bhtc", attention_patterns, value)
+    attention_values = rearrange(context_layer, "b h s c -> b s (h c)")
+    attention_values = project_out(attention_values)
+    return attention_values
+
+
+class AttentionPattern(Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        hidden_size = config["hidden_size"]
+        self.project_query = Linear(hidden_size, hidden_size, bias=True)
+        self.project_key = Linear(hidden_size, hidden_size, bias=True)
+
+    def forward(self, token_activations, attention_masks):
+        return raw_attention_pattern(
+            token_activations=token_activations,
+            num_heads=self.config["num_heads"],
+            project_key=self.project_key,
+            project_query=self.project_query,
+        )
+
+
+class SelfAttentionLayer(Module):
+    def __init__(self, config):
+        super(SelfAttentionLayer, self).__init__()
+        self.config = config
+        if config["hidden_size"] % config["num_heads"] != 0:
+            raise AssertionError("head num must divide hidden size")
+        hidden_size = config["hidden_size"]
+        self.pattern = AttentionPattern(config)
+        self.project_value = Linear(hidden_size, hidden_size, bias=True)
+        self.project_out = Linear(hidden_size, hidden_size, bias=True)
+        self.dropout = Dropout(config["dropout"])
+
+    def forward(self, token_activations, attention_masks=None):
+        return multi_head_self_attention(
+            token_activations,
+            # attention_masks,
+            self.config["num_heads"],
+            self.pattern(token_activations, attention_masks),
+            self.project_value,
+            self.project_out,
+            self.dropout,
+        )
+
+
+class BertBlock(Module):
+    def __init__(self, config):
+        super(BertBlock, self).__init__()
+
+        self.config = config
+        hidden_size = config["hidden_size"]
+        self.layer_norm = LayerNorm((hidden_size,))
+        self.dropout = Dropout()
+        self.attention = SelfAttentionLayer(config)
+
+        self.residual = NormedResidualLayer(
+            config["hidden_size"], config["intermediate_size"], config["dropout"]
+        )
+
+    def forward(self, token_activations, attention_masks=None):
+        attention_output = self.layer_norm(
+            token_activations
+            + self.dropout(self.attention(token_activations, attention_masks))
+        )
+
+        return self.residual(attention_output)
+
+
+class BertLMHead(Module):
+    def __init__(self, config):
+        super(BertLMHead, self).__init__()
+        hidden_size = config["hidden_size"]
+        self.mlp = Linear(hidden_size, hidden_size, bias=True)
+        self.unembedding = Linear(hidden_size, config["vocab_size"], bias=True)
+        self.layer_norm = LayerNorm((hidden_size,))
+
+    def forward(self, activations):
+        return self.unembedding(self.layer_norm(gelu(self.mlp(activations))))
+
+
+@dataclass
+class BertOutput:
+    logits: t.Tensor
+    encodings: t.Tensor
+    classification: t.Tensor
+
+
+class Bert(Module):
+    def __init__(self, config, tokenizer=None):
+        super(Bert, self).__init__()
+
+        default_config = {
+            "vocab_size": 28996,
+            "intermediate_size": 3072,
+            "hidden_size": 768,
+            "num_layers": 12,
+            "num_heads": 12,
+            "max_position_embeddings": 512,
+            "dropout": 0.1,
+            "type_vocab_size": 2,
+            "num_classes": 2,
+        }
+        config = {**default_config, **config}
+        self.tokenizer = tokenizer
+        self.config = config
+
+        self.embedding = BertEmbedding(self.config)
+        self.transformer = Sequential(
+            *[BertBlock(self.config) for _ in range(self.config["num_layers"])]
+        )
+        self.lm_head = BertLMHead(config)
+        self.classification_head = Linear(config["hidden_size"], config["num_classes"])
+        self.classification_dropout = Dropout(config["dropout"])
+
+    def forward(self, input_ids, token_type_ids=None):
+
+        if token_type_ids is None:
+            token_type_ids = t.zeros_like(input_ids).to(next(self.parameters()).device)
+
+        embeddings = self.embedding.embed(
+            input_ids=input_ids, token_type_ids=token_type_ids
+        )
+        encodings = self.transformer(embeddings)
+        logits = self.lm_head(encodings)
+        classification = self.classification_head(
+            self.classification_dropout(encodings[:, 0])
+        )
+        return BertOutput(
+            logits=logits, encodings=encodings, classification=classification
+        )
+
+
+def my_bert_from_hf_weights(their_lm_bert=None, config={}):
+    import transformers
+
+    if their_lm_bert is None:
+        their_lm_bert: transformers.models.bert.modeling_bert.BertModel = (
+            transformers.BertForMaskedLM.from_pretrained("bert-base-cased")
+        )
+    model = their_lm_bert.bert
+    tokenizer = transformers.AutoTokenizer.from_pretrained("bert-base-cased")
+    my_model = Bert({**their_lm_bert.config.to_dict(), **config}, tokenizer)
+    # copy embeddings
+    my_model.embedding.position_embedding.weight = (
+        model.embeddings.position_embeddings.weight
+    )
+    my_model.embedding.token_embedding.weight = model.embeddings.word_embeddings.weight
+    my_model.embedding.token_type_embedding.weight = (
+        model.embeddings.token_type_embeddings.weight
+    )
+    copy_weight_bias(my_model.embedding.layer_norm, model.embeddings.LayerNorm)
+
+    my_layers = list(my_model.transformer)
+    official_layers = list(model.encoder.layer)
+    for my_layer, their_layer in zip(my_layers, official_layers):
+        my_layer: BertBlock
+
+        copy_weight_bias(
+            my_layer.attention.pattern.project_key, their_layer.attention.self.key
+        )
+        copy_weight_bias(
+            my_layer.attention.pattern.project_query, their_layer.attention.self.query
+        )
+        copy_weight_bias(
+            my_layer.attention.project_value, their_layer.attention.self.value
+        )
+
+        copy_weight_bias(
+            my_layer.attention.project_out, their_layer.attention.output.dense
+        )
+
+        copy_weight_bias(my_layer.layer_norm, their_layer.attention.output.LayerNorm)
+
+        copy_weight_bias(my_layer.residual.mlp1, their_layer.intermediate.dense)
+        copy_weight_bias(my_layer.residual.mlp2, their_layer.output.dense)
+        copy_weight_bias(my_layer.residual.layer_norm, their_layer.output.LayerNorm)
+
+    copy_weight_bias(
+        my_model.lm_head.mlp, their_lm_bert.cls.predictions.transform.dense
+    )
+    copy_weight_bias(
+        my_model.lm_head.layer_norm, their_lm_bert.cls.predictions.transform.LayerNorm
+    )
+
+    # bias is output_specific, weight is from embedding
+    my_model.lm_head.unembedding.bias = their_lm_bert.cls.predictions.decoder.bias
+    my_model.lm_head.unembedding.weight = model.embeddings.word_embeddings.weight
+    return my_model, their_lm_bert
+
+
+if __name__ == "__main__":
+    my_bert_from_hf_weights()
---
+++ days/w2d1d2/utils.py
@@ -0,0 +1,204 @@
+import itertools
+import re
+from einops.einops import rearrange
+import torch as t
+import numpy as np
+import time
+import importlib
+from torchtyping import patch_typeguard, TensorType
+import math
+
+from PIL import Image
+import requests
+from io import BytesIO
+import torchvision
+
+
+def load_image(url):
+    response = requests.get(url)
+    return torchvision.transforms.ToTensor(Image.open(BytesIO(response.content)))
+
+
+def to_batches(list_of_tensors, batch_size, trim=False):
+    len = list_of_tensors[0].shape[0]
+    batches = []
+    for i in range(0, len, batch_size):
+        if i + batch_size > len and trim:
+            break
+        batches.append([x[i : i + batch_size] for x in list_of_tensors])
+    return batches
+
+
+def itpeek(tensor: t.Tensor):
+    contains_nan = t.any(t.isnan(tensor)).item()
+    contains_inf = t.any(t.isinf(tensor)).item()
+    string = f"SHAPE {tuple(tensor.shape)} MEAN: {'{0:.4g}'.format(t.mean(tensor.float()).cpu().item())} STD: {'{0:.4g}'.format(t.std(tensor.float()).cpu().item())} {'CONTAINS_NAN! ' if contains_nan else ''}{'CONTAINS_INF! ' if contains_inf else ''}VALS [{' '.join(['{0:.4g}'.format(x) for x in t.flatten(tensor)[:10].cpu().tolist()])}{'...' if tensor.numel()>10 else ''}]"
+    return string
+
+
+def tpeek(name: str, tensor: t.Tensor, ret: bool = False):
+    string = f"{name} {itpeek(tensor)}"
+    if ret:
+        return string
+    print(string)
+
+
+class Timer:
+    def __enter__(self):
+        self.start = time.time()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.time()
+        self.interval = self.end - self.start
+        print("took", self.interval)
+
+
+def getprops(obj):
+    props = set(dir(obj))
+    strprops = set(dir(" "))
+    justones = sorted(list(props - strprops))
+    print("props: \n", "\n    ".join(justones))
+
+
+def has_not_null(obj, prop):
+    return hasattr(obj, prop) and (getattr(obj, prop) is not None)
+
+
+def copy_state_identical(from_module, to_module):
+    state_dict = from_module.state_dict()
+    to_state_keys = set(to_module.state_dict().keys())
+    from_state_keys = set(state_dict.keys())
+    from_only = from_state_keys - to_state_keys
+    to_only = to_state_keys - from_state_keys
+    if len(to_only) > 0 or len(from_only) > 0:
+        raise AssertionError(
+            f"{from_only} only in from module, {to_only} only in to module"
+        )
+    to_module.load_state_dict(from_state_keys)
+
+
+def copy_weight_bias(mine, theirs, transpose=False):
+    if transpose:
+        mine.weight = t.nn.Parameter(rearrange(theirs.weight, "a b -> b a"))
+    else:
+        mine.weight = theirs.weight
+
+    theirs_has_bias = has_not_null(theirs, "bias")
+    mine_has_bias = has_not_null(mine, "bias")
+    if theirs_has_bias != mine_has_bias:
+        print(mine.bias)
+        raise AssertionError("yikes")
+    if mine_has_bias and theirs_has_bias:
+        mine.bias = theirs.bias
+
+
+def import_object_from_qualified_name(qname: str):
+    last_period = qname.rindex(".")
+    module_name = qname[:last_period]
+    object_name = qname[last_period + 1 :]
+    module = importlib.import_module(module_name)
+    out = getattr(module, object_name)
+    assert out is not None
+    return out
+
+
+def einsum(string, *tensors):
+    inputs, output = string.split("->")
+    inputs = [x.split() for x in inputs.split(",")]
+    output = output.split()
+
+    if len(tensors) != len(inputs):
+        raise AssertionError(
+            "einsum string must have same number of inputs as input tensors"
+        )
+    dim_sizes = {}
+    for args, tensor in zip(inputs, tensors):
+        if len(args) != len(tensor.shape):
+            raise AssertionError(
+                f"arg string {args} has the wrong length for shape {tensor.shape}."
+            )
+        for arg, dim in zip(args, tensor.shape):
+            if arg in dim_sizes and dim_sizes[arg] != dim:
+                raise AssertionError(
+                    f"dim {arg} has incompatible dimensions {dim_sizes[arg]} and {dim}"
+                )
+            dim_sizes[arg] = dim
+    for arg in output:
+        if arg not in dim_sizes:
+            raise AssertionError(
+                f"output string has a name that doesn't appear in the input: {arg}"
+            )
+    valid_regex = r"^[a-zA-Z0-9_]+$"
+    for arg in itertools.chain(output, *inputs):
+        if not re.match(valid_regex, arg):
+            raise AssertionError(f"invalid identifier {arg}")
+
+    dim_order = list(output)
+    for dim in dim_sizes.keys():
+        if dim not in dim_order:
+            dim_order.append(dim)
+    wide = t.ones([dim_sizes[x] for x in dim_order])
+    for arg, input in zip(inputs, tensors):
+        input_ordered = rearrange(
+            input,
+            f"{' '.join(arg)} -> {' '.join([x if x in arg else '1' for x in dim_order])}",
+        )
+        wide *= input_ordered
+    return wide.sum(dim=tuple(range(len(output), len(dim_order))))
+
+
+if __name__ == "__main__":
+    i1, i2 = t.rand(10, 20, 40), t.rand(10, 40, 50)
+    print(
+        t.allclose(
+            einsum("batch seq chan, batch chan chan2 -> batch seq chan2", i1, i2),
+            t.einsum("bsc,bct->bst", i1, i2),
+        )
+    )
+
+# prior over sequence lenghts is exponentially decaying, 50% chance below 32 - e^(-x/32)
+# p(b) = e^(-x/32)/32
+# D(x) = 1-e^(-x/32)
+# 1-D(x) = e^(-x/32)
+# -x/32 = ln(1-D(x))
+# x = -32ln(1-D(x))
+
+from einops import *
+
+
+def find_max_batch_size(fn, example_dp):
+    def test_fn(bs):
+        dp = t.as_strided(
+            example_dp, (int(bs), *example_dp.shape), (0, *example_dp.stride())
+        )
+        try:
+            fn(dp)
+            return True
+        except Exception as e:
+            if "CUDA out of memory" in e:
+                return False
+            raise e
+
+    return dist_bisect(test_fn)
+
+
+def exp_dist(decay=32):
+    return lambda p: -32 * math.log(1 - max(min(p, 0.999), 0.001))
+
+
+# dist is a function from percentile to value
+def dist_bisect(fn, distribution=exp_dist(decay=32), min_difference=1):
+    lo, hi = 0, 1
+    counter = 0
+    while distribution(hi) - distribution(lo) > min_difference and counter < 20:
+        mid = (hi - lo) / 2 + lo
+        mid_place = distribution(mid)
+        worked = fn(mid_place)
+        if worked:
+            lo = mid
+        else:
+            hi = mid
+        print("testing ", mid_place, worked)
+        counter += 1
+    return distribution(lo)
