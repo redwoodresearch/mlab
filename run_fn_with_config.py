681e16385665d55d423e358f4f5c4f5e8cb93a72
Tao Lin <taoroalin@gmail.com>
2022-01-13 10:10:47 -0800

    thang

--- run_fn_with_config.py
+++ run_fn_with_config.py
@@ -1,6 +1,6 @@
 import os
 
-os.system("pip install -r requirements.txt")
+os.system("pip install scikit-learn torchtext pytest einops")
 os.system("pip install -e .")
 
 from comet_ml import Experiment
