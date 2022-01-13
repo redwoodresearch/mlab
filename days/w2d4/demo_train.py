cdcc1e2a415ed7026eb5318c00d2a4dc4d928be4
Tao Lin <taoroalin@gmail.com>
2022-01-13 09:48:23 -0800

    d

--- days/w2d4/demo_train.py
+++ days/w2d4/demo_train.py
@@ -9,9 +9,10 @@ from torch.utils.data import DataLoader, TensorDataset
 @gin.configurable
 class MyModel(nn.Module):
     def __init__(self, hidden_size, n_layers, input_size, output_size):
+        super().__init__()
         self.mlps = nn.Sequential(
-            nn.Linear(input_size, hidden_size)
-            * [nn.Linear(hidden_size, hidden_size) for _ in range(n_layers)],
+            nn.Linear(input_size, hidden_size),
+            *[nn.Linear(hidden_size, hidden_size) for _ in range(n_layers)],
             nn.Linear(hidden_size, output_size),
         )
 
@@ -24,7 +25,9 @@ def train(experiment, batch_size, lr, num_epochs):
     model = MyModel()
     optimizer = t.optim.Adam(model.parameters(), lr)
     X, y = make_moons(n_samples=512, noise=0.05, random_state=354)
-    dataset = DataLoader(TensorDataset(X, y), batch_size=batch_size, shuffle=True)
+    dataset = DataLoader(
+        TensorDataset(X, y[:, None]), batch_size=batch_size, shuffle=True
+    )
     for epoch in num_epochs:
         for batch in dataset:
             loss = t.binary_cross_entropy_with_logits(model(batch[0]), batch[1])
