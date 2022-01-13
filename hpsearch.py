5765957c40472521aa8a66a57d2a8e6eae0f4401
Tao Lin <taoroalin@gmail.com>
2022-01-13 09:41:48 -0800

    d

--- hpsearch.py
+++ hpsearch.py
@@ -331,7 +331,7 @@ def hpsearch(name, fn_path, base_config, search_spec, comet_key, local=False):
 
     if local:
         print("LOCAL TRIAL RUN")
-        my_env = {**task_specs[0]["parameters"], **os.environ}
+        my_env = {"PARAMS": json.dumps(task_specs[0]["parameters"]), **os.environ}
         subprocess.Popen(["python", "run_fn_with_config.py"], env=my_env)
         return
 
