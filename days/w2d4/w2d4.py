b92dc984465aed5de29df49d85b73dcd985d421d
mlab people <mlab@rdwrs.com>
2022-01-14 01:18:39 +0000

    script for running remote hp search

--- days/w2d4/w2d4.py
+++ days/w2d4/w2d4.py
@@ -9,12 +9,12 @@ api_key = "OmfvOU0RmHbt4iMa2WIYGBjBf"
 
 hpsearch(
     "inappropriate ioctl",
-    "days.w2d4.demo_train.train",
-    "days/w2d4/demo_train.gin",
-    {"train.lr": [1e-3, 1e-4, 1e-5], "MyModel.hidden_size": [32, 64], "set_random_seed.seed":[0,1,2,3]},
+    "days.w2d4.train.train",
+    "days/w2d4/config.gin",
+    {"train.lr": [1e-3, 1e-4, 1e-5, 1e-6], "set_random_seed.seed":[0,1,2,3]},
     # We provide an additional Gin-configurable function set_random_seed(seed). This means you can control it in your gin without writing the function yourself
     comet_key=api_key,
-    local = True # set to false after it works locally! 
+    local = False # set to false after it works locally! 
 )
 
 # "set_random_seed.seed":[0,1,2,3]
