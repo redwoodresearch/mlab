df3a73712398779e0aa8e9bb31d5313a9e3a1e4e
timbauman <tbauman@tbauman.com>
2022-01-13 17:40:08 -0800

    hi

--- days/w2d4/bert_hp_search.py
+++ days/w2d4/bert_hp_search.py
@@ -1,13 +1,13 @@
 from hpsearch import hpsearch
 
 hpsearch(
-    "mlps_without_activations_warren_tim",
-    "days.w2d4.demo_train.train",
-    "days/w2d4/demo_train.gin",
+    "bert_warren_tim",
+    "days.w2d4.bert_train.train",
+    "days/w2d4/bert_train.gin",
     {
-        # "lr": [1e-3, 1e-4, 1e-5],
-        "lr": [1e-3],
-        "MyModel.hidden_size": [32, 64],
+        "train.lr": [1e-3, 1e-4, 1e-5],
+        # "lr": [1e-3],
+        # "MyModel.hidden_size": [32, 64],
         "set_random_seed.seed": [0, 1, 2, 3],
     },
     # We provide an additional Gin-configurable function set_random_seed(seed). This means you can control it in your gin without writing the function yourself
--- days/w2d4/bert_train.py
+++ days/w2d4/bert_train.py
@@ -3,30 +3,44 @@ import torch as t
 import torch.nn as nn
 from sklearn.datasets import make_moons
 import gin
-from torch.utils.data import DataLoader, TensorDataset
-from w2d1.bert_sol import BertWithClassify, mapkey
-from w2d1 import bert_tests
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+from days.w2d1.bert_sol import BertWithClassify, mapkey
+from days.w2d1 import bert_tests
 import torchtext
+import random
+import transformers
 
 
 @gin.configurable
 def train(experiment, batch_size, lr, num_epochs):
+    tokenizer = transformers.AutoTokenizer.from_pretrained("bert-base-cased")
     
     my_bert = BertWithClassify(
             vocab_size=28996, hidden_size=768, max_position_embeddings=512, 
             type_vocab_size=2, dropout=0.1, intermediate_size=3072, 
-            num_heads=12, num_layers=12)
+            num_heads=12, num_layers=12, num_classes=2)
     pretrained_bert = bert_tests.get_pretrained_bert()
     mapped_params = {mapkey(k): v for k, v in pretrained_bert.state_dict().items()}
     my_bert.load_state_dict(mapped_params)
-    bert_tests.test_same_output(my_bert, pretrained_bert)
-
+    # bert_tests.test_same_output(my_bert, pretrained_bert) # we don't need this - tim
+    my_bert.to('cuda')
+    my_bert.train()
+    
+    loss = t.nn.CrossEntropyLoss()
     optimizer = t.optim.Adam(my_bert.parameters(), lr)
     data_train, data_test = torchtext.datasets.IMDB(root='.data', split=('train', 'test'))
-    train_dataloader =  DataLoader(data_train, batch_size=batch_size)
+    train_dataloader =  DataLoader(list(data_train), shuffle=True, batch_size=batch_size)
     for epoch in range(num_epochs):
         for batch in train_dataloader:
-            loss = t.binary_cross_entropy_with_logits(my_bert(batch[0]), batch[1]).mean()
-            loss.backward()
+            input_ids = t.tensor(tokenizer(list(batch[1]), padding='longest', max_length=512, truncation=True)['input_ids']).to('cuda')
+            target = t.tensor([1 if x == 'pos' else 0 for x in batch[0]], dtype=t.long).to('cuda')
+            output = my_bert(input_ids)[1]
+            l = loss(output, target)
+            l.backward()
             optimizer.step()
             optimizer.zero_grad()
+            
+    
+    
+
+            
