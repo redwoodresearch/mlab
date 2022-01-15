f9bd3297d01d0f78a648b1ca65fca9bc6ffd87d9
Tao Lin <taoroalin@gmail.com>
2022-01-14 18:26:17 -0800

    pipeline parallel

--- ll_remote_setup.sh
+++ ll_remote_setup.sh
@@ -1,2 +1,3 @@
+echo "$1"
 command="$(<lambda-labs-setup.sh)"
 ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh $1 "$command"
--- switch_to_rdwrs.sh
+++ switch_to_rdwrs.sh
@@ -1,3 +1,5 @@
 echo $1
-ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh $1 "cd ~/mlab; sudo pip install -r requirements.txt"
+ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh $1 "pip install traitlets==5.1.1; cd ~;jupyter lab --collaborative --NotebookApp.token=eacfafffeb --port 8890 --ip 0.0.0.0 &"
+# ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh $1 "cd ~/mlab; sudo apt-get install -y python3-venv; python -m venv mlab_env; pip install -r requirements.txt; pip install traitlets==5.1.1; cd ~;jupyter lab --collaborative --NotebookApp.token=eacfafffeb --port 8890 --ip 0.0.0.0 &"
+
 
