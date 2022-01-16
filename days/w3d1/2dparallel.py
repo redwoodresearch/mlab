7dc802ed8f702e49abfe6ed17cfd91bde81acdc7
Tao Lin <taoroalin@gmail.com>
2022-01-15 17:24:31 -0800

    hi

--- days/w3d1/2dparallel.py
+++ days/w3d1/2dparallel.py
@@ -358,7 +358,7 @@ def start_cluster():  # does gin add the arguments here? crazy
             total_rank = C.stage_dp_sizes_cum[mp_rank] + dp_rank
             remote_procs.append(
                 subprocess.Popen(
-                    f'ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh {ip} "cd ~/mlab; python days/w3d1/2dparallel.py process {mp_rank} {dp_rank} {total_rank}"',
+                    f'ssh -o StrictHostKeyChecking=no -i ~/mlab_ssh {ip} "cd ~/mlab; python days/w3d1/2dparallel.py process {mp_rank} {dp_rank} {total_rank} & echo afterthing"',
                     shell=True,
                 )
             )
@@ -378,6 +378,7 @@ def start_cluster():  # does gin add the arguments here? crazy
 
 
 if __name__ == "__main__":
+    print("hi")
     import hashlib
 
     os.system("touch ~/touchfile")
