e0f8ec377833903dc57817bb813503c95a4d0e33
Lambda User <arthurconmy@gmail.com>
2022-01-17 17:46:58 -0800

    Forward pass

--- days/w3d1/pipeline_parallel.py
+++ days/w3d1/pipeline_parallel.py
@@ -13,6 +13,8 @@ DEVICES = ["cuda:4", "cuda:5", "cuda:6", "cuda:7"]
 MAX_LEN = 128
 BATCH_SIZE = 4
 
+transformers.models.gptj.modeling_gptj.GPTJModel
+
 def load_data(batch_size=BATCH_SIZE, random_seed=0):
     print("loading data")
     tensor_path_tokens = "/home/ubuntu/dm_and_nina/days/w3d1/imdb_tokens.pt"
@@ -29,7 +31,6 @@ def load_data(batch_size=BATCH_SIZE, random_seed=0):
         tokens = tokenizer([_t[1] for _t in data_train], padding="longest", max_length=MAX_LEN, truncation=True)["input_ids"]
         tokens = t.tensor(tokens).long()
         labels = t.tensor([0 if _t[0] == "neg" else 1 for _t in data_train])
-        print(tokens.shape, labels.shape)
 
         t.manual_seed(random_seed)
         perm = t.randperm(tokens.shape[0])
@@ -43,10 +44,24 @@ def load_data(batch_size=BATCH_SIZE, random_seed=0):
         t.save(batched_labels, tensor_path_labels)
     return batches, batched_labels
 
+class GPTJBlock2(t.nn.Module):
+    def __init__(self, block):
+            super(GPTJBlock2, self).__init__()
+            self.block = block 
+    
+    def forward(self, x):
+        x = self.block(x)
+        return x[0]
+
 class GPTJPart(t.nn.Module):
     def __init__(self, model, part_num):
         super(GPTJPart, self).__init__()
+
+        self.part_num = part_num
+
         num_blocks = len(model.transformer.h)
+
+        k = num_blocks // 4
 
         if part_num == 0:
 
@@ -55,7 +70,7 @@ class GPTJPart(t.nn.Module):
             self.layers = t.nn.Sequential(
                 model.transformer.wte,
                 model.transformer.drop,
-                *model.transformer.h[0:num_blocks//4]
+                *[GPTJBlock2(b) for b in model.transformer.h[0:k]]
             )
 
         elif part_num == 1:
@@ -63,7 +78,7 @@ class GPTJPart(t.nn.Module):
             # self.layers = t.nn.Linear(4096, 4096)
 
             self.layers = t.nn.Sequential(
-                *model.transformer.h[num_blocks//4:2*num_blocks//4]
+                *[GPTJBlock2(b) for b in model.transformer.h[k:2*k]]
             )
 
         elif part_num == 2:
@@ -71,7 +86,7 @@ class GPTJPart(t.nn.Module):
             # self.layers = t.nn.Linear(4096, 4096)
             
             self.layers = t.nn.Sequential(
-                *model.transformer.h[2*num_blocks//4:3*num_blocks//4]
+                *[GPTJBlock2(b) for b in model.transformer.h[2*k:3*k]]
             )
 
         elif part_num == 3:
@@ -79,7 +94,7 @@ class GPTJPart(t.nn.Module):
             # self.layers = t.nn.Linear(4096, 2)
             
             self.layers = t.nn.Sequential(
-                *model.transformer.h[3*num_blocks//4:],
+                *[GPTJBlock2(b) for b in model.transformer.h[3*k:]],
                 model.transformer.ln_f,
                 model.score
             )
@@ -91,25 +106,19 @@ def run(
     rank,
     size
 ):
-    print("i'm rank", rank)
-    # model = GPTJPart(full_model, rank)
     model = t.load(f"/home/ubuntu/dm_and_nina/days/w3d1/part_{rank}.pt")
     model.train()
     model.to(DEVICES[rank])
     
-    # optimizer = t.optim.SGD(model.parameters(), lr=1e-4)
-
-    # If rank 0, loads data, splits things, keeps a minibatch
-    # else, listen for a minibatch from rank 1
-    # dataloader = DistributedDataLoader(rank=rank, size=size)
-    # batch_size = dataloader.batch_size
+    optimizer = t.optim.SGD(model.parameters(), lr=1e-4)
 
     num_batches = t.tensor(0).to(DEVICES[rank])
+    labels = t.Tensor()
+    tokens = t.Tensor()
 
     if rank == 0:
         tokens, labels = load_data(batch_size=BATCH_SIZE)
-        # num_batches = t.tensor(tokens.shape[0]).to(DEVICES[rank])
-        # num_batches = t.tensor(5).to(DEVICES[rank])
+        labels = labels.long()
         num_batches = t.tensor(tokens.size(dim=0)).to(DEVICES[rank])
 
     group_all = dist.new_group([0, 1, 2, 3])
@@ -122,27 +131,18 @@ def run(
 
     dist.barrier()
 
-    print('Got to for loop')
-
     for i in range(num_batches.item()):
-
-        print('Got to batch')
 
         dist.barrier()
 
-        print('After barrier')
-
-        label = t.zeros(BATCH_SIZE).to(DEVICES[rank])
+        label = t.zeros(BATCH_SIZE, dtype=t.long).to(DEVICES[rank])
         if rank == 0:
             label = labels[i].to(DEVICES[rank])
         dist.broadcast(label, src = 0, group=group_0_3)
 
         if rank == 0:
-            print('Started rank 0')
-            x = model(tokens[i].to(DEVICES[rank])).logits
+            x = model(tokens[i].to(DEVICES[rank]))
             dist.broadcast(x, src=0, group=group_0_1)
-            
-            print('Finished rank 0')
         if rank == 1:
             x = t.zeros(BATCH_SIZE, 128, 4096).to(DEVICES[rank])
             dist.broadcast(x, src=0, group=group_0_1)
@@ -156,8 +156,8 @@ def run(
         if rank == 3:
             x = t.zeros(BATCH_SIZE, 128, 4096).to(DEVICES[rank])
             dist.broadcast(x, src=2, group=group_2_3)
-            x = model(x).logits
-            loss = t.nn.functional.cross_entropy(x, labels[i].to(DEVICES[rank]))
+            x = model(x)
+            loss = t.nn.functional.cross_entropy(x[:,-1,:], label)
             print(loss.item())
             # loss.backward()
             # optimizer.step()
