3fdaf67069677b2bd5f4accc3e1088a7407c2f19
mlab people <mlab-people@jasdkgljasklgjsalgjklasjdg.com>
2022-01-19 10:24:41 -0800

    Make rl_install.sh branch specific

--- rl_install.sh
+++ rl_install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y python-opengl xvfb
-mkdir  ~/mlab/days/w3d3
-cd ~/mlab/days/w3d3
+mkdir  ~/mlab-rudolf-tony/days/w3d3
+cd ~/mlab-rudolf-tony/days/w3d3
 python3 -m venv rl_env
 source rl_env/bin/activate
 pip install gym[atari]==0.19.0 pyglet matplotlib
