c084899c2565d09e7d1985d9bfccbb2136dbcca2
tao <taoroalin@gmail.com>
2022-01-26 21:22:00 +0000

    not quite working

--- rome/rome.py
+++ rome/rome.py
@@ -2,11 +2,13 @@ import random
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import DefaultDict, Dict, Generator, List, Optional, Tuple
+from numpy import indices
 
 import torch as t
 import transformers
 from black import json
-from einops import repeat
+from datasets import load_dataset
+from einops import rearrange, repeat
 from torch import nn
 
 
@@ -36,18 +38,9 @@ NUM_SUBJECTS_TO_SAMPLE = 5
 # this could actually use unit tests
 
 
-def find_most_activated_layer(
-    tokenizer: transformers.PreTrainedTokenizer,
-    model: nn.Module,
-    layers: List[nn.Module],
-    fact: Fact,
-) -> t.Tensor:
-    """ """
-    model.eval()
-
-    perturbations = generate_random_pertubations(fact, tokenizer)
-
+def inputs(tokenizer: transformers.PreTrainedTokenizer, fact: Fact) -> t.Tensor:
     tokenizer.pad_token = tokenizer.eos_token
+    perturbations = generate_random_pertubations(fact)
     inputs = tokenizer(
         [fact_to_sentence_without_object(f) for f in [fact] + perturbations],
         return_tensors="pt",
@@ -55,6 +48,18 @@ def find_most_activated_layer(
         truncation=True,
     )["input_ids"]
     print(tokenizer.decode(inputs[0]))
+    return inputs
+
+
+def find_most_activated_layer(
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: nn.Module,
+    layers: List[nn.Module],
+    inputs: t.Tensor,
+    fact: Fact,
+) -> t.Tensor:
+    """ """
+    model.eval()
 
     patch_activations: Dict[int, (int, t.Tensor)] = {}
     activations_per_layer: List[t.Tensor] = [t.tensor(0) for _ in range(len(layers))]
@@ -83,7 +88,11 @@ def find_most_activated_layer(
 
         # double check that the answers are indeed different
         if logits[0, -1].argmax() in logits[1:, -1].argmax(dim=-1):
-            print("Some of your perturbations return the same value")
+            print("Some of your perturbations return the target value")
+
+        # double check that the fact returns the right answer
+        if not (" " + fact.object).startswith(tokenizer.decode(logits[0, -1].argmax())):
+            print("Your fact is not in the model")
 
         # save output of activations
         reference_activations = activations_per_layer.copy()
@@ -122,9 +131,7 @@ def find_most_activated_layer(
             handle.remove()
 
 
-def generate_random_pertubations(
-    fact: Fact, tokenizer: transformers.PreTrainedTokenizer
-) -> List[str]:
+def generate_random_pertubations(fact: Fact) -> List[str]:
     """
     Going to be easiest to just substitute different subjects in here.
 
@@ -145,3 +152,182 @@ def fact_to_sentence_without_object(fact: Fact) -> str:
 
 def find_region_of_max_causal_effect(probs: t.Tensor) -> Tuple[int, int]:
     """Takes the output of find_most_activated_layer. Returns point (layer, token_idx)"""
+    best_token_idx = probs.sum(dim=0).argmax().item()
+    best_layer_idx = probs[:, best_token_idx].argmax().item()
+    return (best_layer_idx, best_token_idx)
+
+
+def maximize_activations_at_layer(
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: nn.Module,
+    layers: List[nn.Module],
+    layer_idx: int,
+    token_idx: int,
+    inputs: t.Tensor,
+    new_fact: Fact,
+) -> t.Tensor:
+    epochs = 1000
+
+    inputs = inputs[:1]
+
+    # should i use smarter initialization here? like divide by sqrt n or whatever
+    activations = t.rand((1, 1600)).to(model.device).requires_grad_(True)
+
+    def hook(model: nn.Module, input: t.Tensor, output: t.Tensor) -> Optional[t.Tensor]:
+        # trying to retain grad-ability here
+        output[0][:, token_idx] = t.zeros_like(activations)
+        output[0][:, token_idx] += activations
+        return output
+
+    handle = layers[layer_idx].register_forward_hook(hook)
+
+    # this better only be one token long!
+    target = tokenizer.encode(" " + new_fact.object, return_tensors="pt")[0, 0]
+    target = target.unsqueeze(0).to(model.device)
+    print(tokenizer.decode(target))
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    try:
+        optim = t.optim.Adam(lr=1e-3, params=[activations])
+        for _ in range(epochs):
+            optim.zero_grad()
+            logits = model(inputs.to(model.device)).logits
+            loss = loss_fn(logits[:, -1], target)
+            loss.backward()
+            print(loss)
+            optim.step()
+
+        print(loss)
+        print(t.softmax(logits[0, -1], dim=0).topk(10).indices)
+        print(tokenizer.decode(t.softmax(logits[0, -1], dim=0).topk(10).indices))
+        print(t.softmax(logits[0, -1], dim=0).topk(10).values)
+        return activations
+
+    finally:
+        handle.remove()
+
+
+def find_layer_hidden_state(
+    model: nn.Module,
+    layer_idx: int,
+    token_idx: int,
+    inputs: t.Tensor,
+):
+    inputs = inputs[:1]
+
+    hidden_state = t.zeros((4 * 1600,))
+    # output_to_return = t.zeros(
+    #     (
+    #         6,
+    #         1600,
+    #     )
+    # )
+
+    def hook(model: nn.Module, input: t.Tensor, output: t.Tensor) -> Optional[t.Tensor]:
+        hidden_state[:] = input[0][0, token_idx]
+        # output_to_return[:] = output[0]
+
+    handle = model.transformer.h[layer_idx].mlp.c_proj.register_forward_hook(hook)
+
+    try:
+        with t.no_grad():
+            model(inputs.to(model.device))
+        return hidden_state  # , output_to_return
+    finally:
+        handle.remove()
+
+
+def find_average_inputs_to_layer(
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: nn.Module,
+    layer_idx: int,
+):
+    """Probably gonna want to cache this somewhere for the next time"""
+    batch_size = 8
+    batches = 200
+    model.eval()
+    # load all of fucking wikipedia
+
+    dataset = load_dataset("wikitext", "wikitext-103-v1")
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    hidden_states = []
+
+    def hook(model: nn.Module, input: t.Tensor, output: t.Tensor) -> Optional[t.Tensor]:
+        hidden_states.append(rearrange(input[0], "b s h -> (b s) h"))
+
+    handle = model.transformer.h[layer_idx].mlp.c_proj.register_forward_hook(hook)
+    try:
+        for batch_idx in range(batches):
+            inputs = tokenizer(
+                dataset["train"][batch_idx * batch_size : (batch_idx + 1) * batch_size][
+                    "text"
+                ],
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+            )["input_ids"].to(model.device)
+
+            with t.no_grad():
+                model(inputs.to(model.device))
+    finally:
+        handle.remove()
+
+    K = t.cat(hidden_states, dim=0)
+
+    return t.cov(K.T @ K)
+
+
+def new_weights_from_activation(
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: nn.Module,
+    layer_idx: int,
+    hidden_state: t.Tensor,
+    activations: t.Tensor,
+    C: t.Tensor,
+) -> None:
+    layer = model.transformer.h[layer_idx]
+    weights = layer.mlp.c_proj.weight
+
+    k_star = hidden_state.to(model.device)
+    v_star = activations.to(model.device)
+
+    u_T = t.einsum("ij, j -> i", t.inverse(C), k_star)
+
+    I_concat = t.cat(
+        [t.eye(k_star.shape[0]).to(model.device), k_star.unsqueeze(1)], dim=1
+    )
+    u_T_plus_zero = t.cat([u_T, t.zeros((1,)).to(model.device)], dim=0).unsqueeze(0)
+    I_concat = t.cat([I_concat, -u_T_plus_zero], dim=0)
+
+    W_hat_plus_v = t.einsum(
+        "ij, ik -> kj", t.cat([weights, v_star], dim=0), t.inverse(I_concat)
+    )
+
+    return W_hat_plus_v[:-1, :]
+
+    # another method
+    # v = W_hat_plus_v[-1, :]
+    # other_W_hat = weights + t.einsum("i, j -> ij", v, u_T).T
+    # return other_W_hat
+
+
+def test_on_fact(
+    model: nn.Module, tokenizer: transformers.PreTrainedTokenizer, fact: Fact
+) -> str:
+    tokenizer.pad_token = tokenizer.eos_token
+    inputs = tokenizer(
+        [fact_to_sentence_without_object(fact)],
+        return_tensors="pt",
+        padding=True,
+        truncation=True,
+    )["input_ids"].to(model.device)
+
+    model.eval()
+    with t.no_grad():
+        logits = model(inputs.to(model.device)).logits
+    return logits
+
+    # return tokenizer.decode(logits[0, -1].argmax(dim=0))
